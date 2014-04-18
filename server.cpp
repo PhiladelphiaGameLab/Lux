e9302d2d2d36d0e4fe4d4dbfe1b4790dcb0f37a7
jra59 <jake@philadelphiagamelab.org>
2014-04-18 16:27:10 -0400

    A version of server that compiles, but lacks all threads for now

--- server.cpp
+++ server.cpp
@@ -19,30 +19,30 @@ void error(char *msg){
 
 
 int main(int argc, char *argv[]){
-	if (argc < 2) {
-      		std::cout << "ERROR, no port provided" << std::endl;
-      		exit(0);
-   	}
-
-	int newsockfd, sock;
+	int newsockfd, sock, n;
 	int clilen; // stores the size of the client
 	socklen_t fromlen; // number of characters read/written
 	
-	char buffer[MESSAGE_SIZE]; // server reads input to this buffer
+	char buf[MESSAGE_SIZE]; // server reads input to this buffer
  	
 
 	struct sockaddr_in serv_addr, cli_addr; // server/client addresses
 
 	if(socket(AF_INET, SOCK_DGRAM, 0) < 0){
-		error("Error Opening Socket");
+		error("socket");
 	}
 	
 	bzero((char *) &serv_addr, sizeof(serv_addr));
 	serv_addr.sin_family = AF_INET;
 	serv_addr.sin_addr.s_addr = INADDR_ANY;
-	serv_addr.sin_port = hton(atoi(argv[1]));
+	if(argc < 1){
+		serv_addr.sin_port = htons(atoi(argv[1]));
+	}else{
+		serv_addr.sin_port = htons(3000);
+	}
+	printf("%d", serv_addr.sin_port);
 	// establish a socket to listen on using a cmd line arg	
-	if (bind(sock,(struct sockaddr *)&serv_addr,sizeof(serv_addr))<0){ 
+	if (bind(sock,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0){ 
       		 error("binding");
 	}
 	// spawn BGT(s)
@@ -51,14 +51,14 @@ int main(int argc, char *argv[]){
 	fromlen = sizeof(struct sockaddr_in);
 	while(1){
 		// accept clients, send them the port address they should be on
-		if(n = recvfrom(sock,buf,1024,0,(struct sockaddr *)&cliaddr,&fromlen) < 0){
-			error("Could not recieve on socket");
+		if(n = recvfrom(sock,buf,MESSAGE_SIZE,0,(struct sockaddr *)&cli_addr,&fromlen) < 0){
+			error("Receive");
 		}
 		write(1,"Received a datagram: ",21);
        		write(1,buf,n);
 
 		if(n = sendto(sock,"Got your message\n",17,0,(struct sockaddr *)&cli_addr,fromlen) < 0){
-			error("Could not send to the socket");
+			error("send");
 		}
 		// clients should send their client ID/token to establish identity
 		
@@ -66,7 +66,7 @@ int main(int argc, char *argv[]){
 	}
 	return 0;
 }
-
+/*
 void battleGroundThread(struct *BGT){
 	// initialize linked list of sockets on connection
 	// initialize the broadcast queue
@@ -83,7 +83,7 @@ void battleGroundThread(struct *BGT){
 }
 
 void broadcastThread(struct broadcast){
-	while(1){/*
+	while(1){
 		if(!*broadcast->BroadcastQueue.empty()){ // if there is something in the broadcast queue 
 			for (std::list<TCPServerSocket>::iterator socket = *broadcast->SocketList.begin(); socket != *broadcast->SocketList.end(); socket++){
 				*socket->send(*broadcast->BroadcastQueue.front());
@@ -91,5 +91,5 @@ void broadcastThread(struct broadcast){
 			*broadcast->BroadcastQueue.pop();
 		}
 		// otherwise we can probably give up our processor time. 
-	*/	}
-}
+		}
+}*/
