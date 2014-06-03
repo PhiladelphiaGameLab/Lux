011e677917fe43965cdb27440b775a0f015aeba5
Paul Lee <paul.lee11@gmail.com>
2014-06-03 15:21:16 -0400

    Update FindBGT.cpp

--- src/classes/static/FindBGT.cpp
+++ src/classes/static/FindBGT.cpp
@@ -1,40 +1,57 @@
 #include "FindBGT.h"
 
-s_BGTInfo FindBGT::find(BSONObj clientDocument, BSONObj BGTDocument) {
+s_BGTInfo FindBGT::find(BSONObj clientDocument) {
 
   // get accessToken from BSONObj clientDocument
   std::string accessToken = clientDocument["sender"]["accessToken"].String();
   // get EUID from BSONObj clientDocument
   std::string EUID = clientDocument["sender"]["EUID"].String();
   
-  // get accessToken from BSONObj BGTDocument
-  std::string BGTAcessToken = BGTDocument["sender"]["accessToken"].String();
-  // get EUID from BSONObj BGTDocument
-  std::string EUID = BGTDocument["sender"]["EUID"].String();
-  
-  //create structure 
+  // create a s_BGTInfo struct 
   s_BGTInfo BGTInfo;
       
   if (Authenticate::authenticateAccessToken(accessToken, EUID)) {
     
-    //pulls x-location and y-location from clientDocument
+    // pull x-location and y-location from clientDocument
     int location[0] = atoi(clientDocument["object"]["location"]["x"].String().c_str());
     int location[1] = atoi(clientDocument["object"]["location"]["y"].String().c_str());
     
-    //assigns proper value to BGTInfo
+    // assign proper value to BGTInfo.bgt_id
     BGTInfo.bgt_id = HMBL::findBucket(location[0], location[1]);
-    
-    //pulls radius, mapX, mapY, columns, and rows from BGTDocument to use as parameters in HMBL.surroundings
-    int radius = atoi(BGTDocument["object"]["radius"].String().c_str());
-    int mapX = atoi(BGTDocument["object"]["mapX"].String().c_str());
-    int mapY = atoi(BGTDocument["object"]["mapY"].String().c_str());
-    int columns = atoi(BGTDocument["object"]["columns"].String().c_str());
-    int rows = atoi(BGTDocument["object"]["rows"].String().c_str());
-    
-    BGTInfo.buckets = HMBL.surroundings(location[0], location[1], radius, mapX, mapY, columns, rows);
+    // assign proper value to BGTInfo.buckets
+    BGTInfo.buckets = FindBGT::findSurroundings(clientDocument, BGTInfo.bgt_id);
     
   }
 
   return BGTInfo;
   
 }
+
+//-----------------------------------HELPER FUNCTION-----------------------------------
+
+std::vector<int> FindBGT::findSurroundings(BSONObj clientDocument, int bgt_id) {
+  
+    // connect to the database
+    mongo::DBClientConnection c;
+    c.connect("localhost")
+    
+    // find the BGT Document
+    BSONObj BGTDoc = c.findOne(DATABASE_NAME, Query("Type" << "BGT" << "BGT_ID" << bgt_id));
+       
+    // pull x-location and y-location from clientDocument
+    int location[0] = atoi(clientDocument["object"]["location"]["x"].String().c_str());
+    int location[1] = atoi(clientDocument["object"]["location"]["y"].String().c_str());
+    
+    // pull radius, mapX, mapY, columns, and rows from BGTDocument to use as parameters in HMBL::surroundings
+    int radius = atoi(BGTDoc["object"]["radius"].String().c_str());
+    int mapX = atoi(BGTDoc["object"]["mapX"].String().c_str());
+    int mapY = atoi(BGTDoc["object"]["mapY"].String().c_str());
+    int columns = atoi(BGTDoc["object"]["columns"].String().c_str());
+    int rows = atoi(BGTDoc["object"]["rows"].String().c_str())
+    
+    return HMBL::surroundings(location[0], location[1], radius, mapX, mapY, columns, rows);
+
+}
+
+
+
