3f95be36bb4c8597b0c82a03a5aad26d12e7fedb
Jake <Alpha59@users.noreply.github.com>
2014-06-06 15:12:13 -0400

    Update battleground.cpp

--- src/processes/battleground.cpp
+++ src/processes/battleground.cpp
@@ -1,21 +1,20 @@
 /*
- * This code was orginally written and developed by the Lux Backend Team
- * at Philadelphia Game Lab:
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2 of the License, or (at your
- * option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
- * more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program. If not, see <http://www.gnu.org/licenses/>.
- *
- * - Jake
+read the message from the socket
+get the access token & EUID
+Authenticate EUID against Access Token
+	strip access token
+	add timestamp
+	check for _id
+		insert into mongo
+	get location from message
+	check if EU_doc || cli_obj_doc
+		EU_doc:
+			update in HMBL
+		get the bucket 
+		update the bucket in the document
+		get the relevent socket list of the new bucket
+		send to send updates thread
+		append the client_socket
  */
 
 #include "battleground.h"
