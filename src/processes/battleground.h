8a31a8896cfbde34fdd6d9e1e94e5c952887c10d
Jake <Alpha59@users.noreply.github.com>
2014-06-10 15:54:19 -0400

    Create battleground.h

---
+++ src/processes/battleground.h
@@ -0,0 +1,52 @@
+/*
+ * This code was orginally written and developed by the Lux Backend Team
+ * at Philadelphia Game Lab:
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * - Jake
+ */
+#ifndef BATTLEGROUND_H
+#define BATTLEGROUND_H
+
+
+// static class (thread)
+
+typedef struct s_SUT{
+    const char *pipeLocation;
+};
+
+typedef struct s_BGT{
+    int port;
+};
+
+typedef struct s_bgt_params_in
+{
+    char* pipe_w;
+}
+
+typedef struct s_SUTMessage
+{
+    BSONobj message;
+    std::vector<Node *> SocketList;
+}
+class BattleGround{
+    public:
+        static void spawn(void *param);
+        uint16_t portNo = getNewPort();
+    protected:
+    private:
+};
+
+#endif // BATTLEGROUND_H
