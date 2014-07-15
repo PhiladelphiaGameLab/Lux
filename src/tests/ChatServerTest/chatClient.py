20c1f6e05de034e851359bb1c039e1b345a87302
Yan <kexuyan@live.cn>
2014-07-15 00:24:15 -0400

    fixed send message

--- src/tests/ChatServerTest/chatClient.py
+++ src/tests/ChatServerTest/chatClient.py
@@ -5,33 +5,33 @@ import threading
 import time
 
 BUFSIZE = 65507
-global sock
-global serverAddr
-chatting = False
-displayLock = threading.Lock()
-historyLock = threading.Lock()
+global gSock
+global gServerAddr
+gChatting = False
+gDisplayLock = threading.Lock()
+gHistoryLock = threading.Lock()
 
-recent = ''
-history = []
+gRecent = ''
+gHistory = []
 
-width = 66
+gWidth = 66
 
-msgId = 0
-global euid
-recvPort = 0
-pollPort = 0
+gMsgId = 0
+global gEuid
+gRecvPort = 0
+gPollPort = 0
 
 class MyThread(threading.Thread):    
     def __init__(self):
         threading.Thread.__init__(self)
 
     def run(self):
-        global sock
+        global gSock
         global BUFSIZE
-        global recent
+        global gRecent
         while True:
-            data, addr = sock.recvfrom(BUFSIZE)
-            recent = "receive message\n"
+            data, addr = gSock.recvfrom(BUFSIZE)
+            gRecent = "receive message\n"
             parseMessage(data)
             display()
 
@@ -64,9 +64,9 @@ class MESSAGE_TYPE:
     CREATE_CHAT_INVALID_USER = 15
                         
 def display():
-    displayLock.acquire()
+    gDisplayLock.acquire()
     os.system('cls' if os.name == 'nt' else 'clear')
-    if chatting:
+    if gChatting:
         print "==============================================================="
         print "                          Chat room                            "
         print "==============================================================="
@@ -82,22 +82,22 @@ def display():
         print "0. quit"
         print "==============================================================="
 
-    print recent
+    print gRecent
     
-    for data in history:
+    for data in gHistory:
         formatPrint(data)
     
-    displayLock.release()
+    gDisplayLock.release()
 
 def formatPrint(data):
     prefix = ''
     if data[0] == "Me:":
-        prefix = ' ' * (width / 2 - 3) + ' | ';
+        prefix = ' ' * (gWidth / 2 - 3) + ' | ';
     print prefix, data[0]
 
     length = len(data[1])
     start = 0
-    half = width / 2 - 4
+    half = gWidth / 2 - 4
     
     while length > 0:
         sublength = half
@@ -108,13 +108,13 @@ def formatPrint(data):
         length -= sublength
     
 def mainLoop():
-    global sock
+    global gSock
     while True:
         display()
         data = raw_input()
         if len(data) > 0:
             f = ord(data[0]) - ord('0')
-            if not chatting:
+            if not gChatting:
                 if f == 0:
                     print "Bye..."
                     exit()
@@ -131,78 +131,59 @@ def mainLoop():
 
 def int32ToBytes(number):
     arr = bytearray()
-    arr += chr((number >> 24) & 0xff)
-    arr += chr((number >> 16) & 0xff)
-    arr += chr((number >> 8)  & 0xff)
     arr += chr(number & 0xff)
+    arr += chr((number >> 8) & 0xff)
+    arr += chr((number >> 16)  & 0xff)
+    arr += chr((number >> 24)& 0xff)
     return arr
 
 def int16ToBytes(number):
     arr = bytearray()
-    arr += chr((number >> 8) & 0xff)
     arr += chr(number & 0xff)
+    arr += chr((number >> 8) & 0xff)
     return arr
                     
 def connect():
-    global recent
-    recent = "connect to server..."
+    global gRecent
+    gRecent = "connect to server..."
     msg = bytearray()
-    msg += int16ToBytes(recvPort)
-    msg += int16ToBytes(pollPort)
-    packet = makePacket(euid, REQUEST_TYPE.CONNECT,
+    msg += int16ToBytes(gRecvPort)
+    msg += int16ToBytes(gPollPort)
+    packet = makePacket(gEuid, REQUEST_TYPE.CONNECT,
                         MESSAGE_TYPE.PORTS, msg)
     sendtoServer(packet)
 
 def chat():
     print "chat"
-    global chatting
-    chatting = True
+    global gChatting
+    gChatting = True
 
 def sendMessage(data):
-    global recent
-    recent = "sent message" 
+    global gRecent
+    gRecent = "sent message" 
     saveMessage(("Me:", data))
-    
 
 def saveMessage(data):
-    historyLock.acquire()
+    gHistoryLock.acquire()
 
-    global history
-    history.append(data)
+    global gHistory
+    gHistory.append(data)
     
-    historyLock.release()
+    gHistoryLock.release()
 
 def makePacket(senderId, reqType, msgType, msg):
-    global msgId
-    msgId += 1
+    global gMsgId
+    gMsgId += 1
     packet = bytearray()
-    packet += int32ToBytes(msgId)
+    packet += int32ToBytes(gMsgId)
     packet += str.encode(senderId)
     packet += chr(reqType)
     packet += chr(msgType)
-    print "p", msg[0] << 8 | msg[1]
-    print "p", msg[2] << 8 | msg[3]
-    print msg[0]
-    print msg[1]
-    print msg[2]
-    print msg[3]
-    print "==="
-    for x in msg:
-        print x
-        packet += chr(x)
-    print "==="
-    print packet[8]
-    print packet[9]
-    print packet[10]
-    print packet[11]
-    
+    packet += msg;
     return packet
     
 def sendtoServer(buf):    
-    sock.sendto(buf, serverAddr)
-    data, addr = sock.recvfrom(BUFSIZE)
-    global recent
-    recent = "receive"
+    gSock.sendto(buf, gServerAddr)
 
 if __name__ == "__main__":
     argc = len(sys.argv)
@@ -210,24 +191,24 @@ if __name__ == "__main__":
         print "usage:", sys.argv[0], "<hostname> <hostport> <EUID>(2byte)"
         sys.exit(1)
 
-    global euid
-    global serverAddr
-    global sock
+    global gEuid
+    global gServerAddr
+    global gSock
     
     # get command line inputs
     hostName = sys.argv[1]
     hostPort = sys.argv[2]
-    euid = sys.argv[3]
-    serverAddr = (hostName, int(hostPort))
-    sock = socket.socket(socket.AF_INET, # Internet
+    gEuid = sys.argv[3]
+    gServerAddr = (hostName, int(hostPort))
+    gSock = socket.socket(socket.AF_INET, # Internet
                          socket.SOCK_DGRAM) # UDP
-    sock.bind(('localhost', 5000)) #let OS to pick up an open port to bind with
+    gSock.bind(('localhost', 0)) #let OS to pick up an open port to bind with
 
-    recvPort = pollPort = int(sock.getsockname()[1]);
+    gRecvPort = gPollPort = int(gSock.getsockname()[1]);
 
-    #receiving = MyThread()
-    #receiving.daemon = True
-    #receiving.start()
+    receiving = MyThread()
+    receiving.daemon = True
+    receiving.start()
 
     mainLoop();
 
