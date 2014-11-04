c2ab9167b413b636c6a690654e1d4f3add3b7d5a
Alex Cannon <acannon828@gmail.com>
2014-11-04 16:08:01 -0500

    added initial upload script

---
+++ FileUpload/upload.php
@@ -0,0 +1,28 @@
+<form action="upload.php" method="post" enctype="multipart/form-data">
+  Please choose a file: <input type="file" name="uploadFile"><br>
+  <input type="submit" value="Upload File">
+</form>
+
+<?php
+$target_dir = "uploads/";
+$target_dir = $target_dir . basename( $_FILES["uploadFile"]["name"]);
+$uploadOk=1;
+
+// Check if file already exists
+if (file_exists($target_dir . $_FILES["uploadFile"]["name"])) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else { 
+    if (move_uploaded_file($_FILES["uploadFile"]["tmp_name"], $target_dir)) {
+        echo "The file ". basename( $_FILES["uploadFile"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+?>
