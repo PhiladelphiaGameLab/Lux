fb1756ea32ba3fd0b84b3dd943f29b0c754ca1e4
CharlesSPatterson <charlesspatterson@gmail.com>
2015-01-10 14:28:38 -0500

    some scoreboard functions

---
+++ scoreboard2/removeuser.php
@@ -0,0 +1,21 @@
+<?php
+
+include_once('../Core/lux-functions.php');
+include_once('../Core/output.php');
+include_once('../Core/db.php');
+include_once('../Core/auth.php');
+
+$db = new Db();
+$OUTPUT = new Output();
+$collection = $db->selectCollection("Scoreboard");
+$LF = new LuxFunctions();
+$AUTH = new Auth();
+$userID = $AUTH->getClientId();
+
+$query = array(
+    "userID" => $LF->fetch_avail("userID")
+);
+
+$results = $collection->remove($query, array("justOne" => true));
+
+$OUTPUT->success("success", $results);
---
+++ scoreboard2/resetuser.php
@@ -0,0 +1,29 @@
+<?php
+
+include_once('../Core/lux-functions.php');
+include_once('../Core/output.php');
+include_once('../Core/db.php');
+include_once('../Core/auth.php');
+
+$db = new Db();
+$OUTPUT = new Output();
+$collection = $db->selectCollection("Scoreboard");
+$LF = new LuxFunctions();
+$AUTH = new Auth();
+$userID = $AUTH->getClientId();
+
+$query = array(
+    "userID" => $LF->fetch_avail("userID")
+);
+
+$update = array(
+    $set => array(
+        "score" => 0,
+        "levels" => array(),
+        "assets" => array()
+    )
+);
+
+$results = $collection->update($query, $update);
+
+$OUTPUT->success("success", $results);
