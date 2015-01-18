811f066eabdf8ea75af997fac22bb90ca1700367
Brad Palchesko <bradpalchesko@gmail.com>
2015-01-18 18:41:15 -0500

    Transactions implemented, testing, param checks

--- Transaction/Transaction.php
+++ Transaction/Transaction.php
@@ -6,11 +6,13 @@
 	function createTransactionQuery($id) {
 		$query_str = $_SERVER['QUERY_STRING'];
 		parse_str($query_str, $query);
+		unset($query["acc_token"]);
 		$query["id0"] = $id;
+		unset($query["method"]);
+		if(isset($query["testing"])) {
+			unset($query["testing"]);
+		}
 		if(!completeQueryString($query)) {
-			echo "Incomplete transaction details. Query string
-			must include id0, id1, subId0, subId1, itemsId0, 
-			and itemsId1.";
 			return NULL;
 		}
 		return $query;
@@ -21,71 +23,83 @@
 	 * information: id0, id1, subId0, subId1, itemsId0, and itemsId1.
 	 */
 	function completeQueryString($query) {
-		$complete = !empty($query["id0"]) && !empty($query["id1"]) &&
-					!empty($query["subId0"]) && !empty($query["subId1"]) &&
-					!empty($query["itemsId0"]) && !empty($query["itemsId1"]);
-		if(!$complete) {
-			echo "Incomplete transaction details. Query string
-			must include id0, id1, subId0, subId1, itemsId0, 
-			and itemsId1.";
-		}
-		return $complete;
+		if(isset($query["itemsId0"]) && !is_array($query["itemsId0"])) return false;
+		if(isset($query["itemsId1"]) && !is_array($query["itemsId1"])) return false;
+		return !empty($query["id0"]) && !empty($query["id1"]) &&
+				!empty($query["subId0"]) && !empty($query["subId1"]) &&
+				(isset($query["itemsId0"]) && is_array($query["itemsId0"]) || 
+					isset($query["itemsId1"]) && is_array($query["itemsId1"]));
 	}
 
 	/**
 	 * Compares two transaction queries. The fields should be mirrored,
 	 * so id0 of $query0 should correspond to id1 of $query1.
 	 */
-	function compareTransactions($query0, $query1) {
+	function matchTransactions($query0, $query1) {
+		$query0items0 = null;
+		if(isset($query0["itemsId0"]) && is_array($query0["itemsId0"])) $query0items0 = $query0["itemsId0"];
+		$query0items1 = null;
+		if(isset($query0["itemsId1"]) && is_array($query0["itemsId1"])) $query0items1 = $query0["itemsId1"];
+		$query1items0 = null;
+		if(isset($query1["itemsId0"]) && is_array($query1["itemsId0"])) $query1items0 = $query1["itemsId0"];
+		$query1items1 = null;
+		if(isset($query1["itemsId1"]) && is_array($query1["itemsId1"])) $query1items1 = $query1["itemsId1"];
+
 		return strcmp($query0["id0"], $query1["id1"]) == 0 &&
 			strcmp($query0["id1"], $query1["id0"]) == 0 &&
 			strcmp($query0["subId0"], $query1["subId1"]) == 0 &&
 			strcmp($query0["subId1"], $query1["subId0"]) == 0 &&
-			compareItemArrays($query0["itemsId0"], $query1["itemsId1"]) &&
-			compareItemArrays($query0["itemsId1"], $query1["itemsId0"]);		
+			compareItemArrays($query0items0, $query1items1) &&
+			compareItemArrays($query0items1, $query1items0);		
+	}
+
+	function flipTransaction($transaction) {
+		$flipped = array(		
+			"id0" => $transaction["id1"],
+			"id1" => $transaction["id0"],
+			"subId0" => $transaction["subId1"],
+			"subId1" => $transaction["subId0"]);
+		if(isset($transaction["itemsId1"])) {
+			$flipped["itemsId0"] = $transaction["itemsId1"];
+		}
+		if(isset($transaction["itemsId0"])) {
+			$flipped["itemsId1"] = $transaction["itemsId0"];
+		}
+		if(isset($transaction["type"])) {
+			$flipped["type"] = $transaction["type"];
+		}
+		return $flipped;
 	}
 
 	/**
 	 * Returns whether two arrays of items are identical.
 	 */
 	function compareItemArrays($items0, $items1) {
-		return empty(array_diff($items0, $items1)) &&
-			empty(array_diff($items1, $items0));
-	}
-
-
-	/**
-	 * Yields the id, subaccount id, and items for the given party.
-	 * Parameter 0 for id0 and 1 for id1.  
-	 */
-	function getPartyDetails($query, $party) {
-		if($party) {
-			return array(
-				"id" => $query["id1"],
-				"sub" => $query["subId1"],
-				"items" => $query["itemsId1"]);
+		if($items0 == null && $items1 ==  null) {
+			return true;
+		} else if($items0 == null || $items1 ==  null) {
+			return false;
 		} else {
-			return array(
-				"id" => $query["id0"],
-				"sub" => $query["subId0"],
-				"items" => $query["itemsId0"]);
+			return empty(array_diff($items0, $items1)) && empty(array_diff($items1, $items0));
 		}
-	}	
+	}
 
 	/**
 	 * Printing for testing
 	 */
 	function printQuery($query) {
-		foreach ($query as $key => $value) {
-   			if(is_array($value)) {
-   				echo $key . ": ";
-   				for($i = 0; $i < count($value) - 1; $i++) {
-  					echo $value[$i]. ", ";
-  				}
-  				echo $value[count($value) - 1]. "<br>";
-    		} else {
-        		 echo $key . ": " . $value . "<br>";
-    		}
+		if(is_array($query)) {
+			foreach ($query as $key => $value) {
+	   			if(is_array($value)) {
+	   				echo $key . ": ";
+	   				for($i = 0; $i < count($value) - 1; $i++) {
+	  					echo $value[$i]. ", ";
+	  				}
+	  				echo $value[count($value) - 1]. "<br>";
+	    		} else {
+	        		 echo $key . ": " . $value . "<br>";
+	    		}
+			}
 		}
 	}
 
