26e889670a26cc5ef9be13e53008bccf8e469977
EC2 Default User <ec2-user@ip-172-31-23-120.ec2.internal>
2015-02-04 18:08:55 +0000

    added phonics specific functions to scoreboard

---
+++ Scoreboard/adjustlesson.php
@@ -0,0 +1,40 @@
+<?php
+
+include_once('../Core/lux-functions.php');
+include_once('../Core/output.php');
+include_once('../Core/db.php');
+include_once('../Core/auth.php');
+
+$db = new Db();
+$OUTPUT = new Output();
+$collection = $db->selectCollection("Scoreboard");
+$LF = new LuxFunctions();
+$AUTH = new Auth();
+
+$results = $collection->update(
+    array(
+	"userId"=> $AUTH->getClientId(),
+	"levels.levelID" => $LF->fetch_avail("levelID")
+	),  
+    array('$set'=>array(
+		"score"=>$LF->fetch_avail("score"),
+		"completed"=>$LF->is_avail("completed")? $LF->fetch_avail("completed") : false
+	)),
+    array("upsert" => true)
+);
+if(isset($results["writeError"])){
+	$results = $collection->update(
+	    array(
+		"userId"=> $AUTH->getClientId(),
+		),  
+	    array('$push'=> array("levels"=>
+				array(
+					"score"=>$LF->fetch_avail("score"),
+					"completed"=>$LF->is_avail("completed")? $LF->fetch_avail("completed") : false
+					"levelID" => $LF->fetch_avail("levelID")
+				)
+			),  
+		));
+}
+
+$OUTPUT->success("Successfully Updated Level", $results);
