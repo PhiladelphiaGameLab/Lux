2a5ff431736880774cab46cb2309b64fcfe26ece
Alpha59 <jake@philadelphiagamelab.org>
2015-04-06 19:45:12 +0000

    Added the Template Rendering for basic HTML templates, no translation from other mark-up languages

---
+++ Templates/getTemplate.php
@@ -0,0 +1,55 @@
+<?php
+include_once('../Core/lux-functions.php');
+include_once('../Core/output.php');
+include_once('../Core/db.php');
+include_once('../Core/auth.php');
+
+function getArbitrary($results, $fields){
+	if(count($fields) == 1){
+		return $results[array_shift($fields)];
+	}else{
+		$rest = $results[reset($fields)];
+		$field_new = array_shift($fields);
+		return getArbitrary($rest, $fields);
+	}
+}
+
+function getField($field){
+	$fields = explode(".",$field);
+	$query = array("field" => $fields[0]); 
+	$db = new Db("System");
+	$collection = $db->selectCollection("CMS");
+	$results = $collection->findOne($query);
+	array_shift($fields);
+	return getArbitrary($results, $fields);
+}
+	
+
+function getPage($template){
+	$re = "/{{(.*)}}/miU"; 
+	$content = file_get_contents( "./html/".$template, true );
+	preg_match_all($re, $content, $matches, PREG_PATTERN_ORDER);
+	$content_values = array();
+	foreach($matches[1] as $value){
+		$content_values[] = getField($value);
+	}
+	return str_replace($matches[0], $content_values, $content);
+
+}
+
+
+$LF = new LuxFunctions();
+
+if(is_array($LF->fetch_avail("template"))){
+	foreach($LF->fetch_avail("template") as $value){
+		$replaced = getPage($value);
+		echo $replaced;
+	}
+}else{
+	$replaced = getPage($LF->fetch_avail("template"));
+	echo $replaced;
+}
+
+
+?>
+
---
+++ Timer/README.md
@@ -0,0 +1,18 @@
+# Timer Service
+The timer Service creates an instance of a clock on a server which can be referenced as a "game time" or other use of standard timing (auction, ect) in order to determine when something expires according to server time. Timer callbacks are messages that can be published to the real-time script.
+
+## API
+### Create timer
+* w/ callback
+* periodic timer (infinite or finite)
+
+### update timer
+### start/stop timer
+### delete timer
+### clear periodic
+### reset periodic
+### check time
+
+### References
+[Reference](https://api.dartlang.org/apidocs/channels/stable/dartdoc-viewer/dart:async.Timer)
+
